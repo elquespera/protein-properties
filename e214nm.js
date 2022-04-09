f1c72d2ff667b2e7d21bdd3fd0bb363b61d2dfc0
elquespera <pavel.grinkevich@gmail.com>
2022-04-09 13:54:51 +0200

    fix: molecular weight calculations

--- e214nm.js
+++ e214nm.js
@@ -1,37 +1,38 @@
-const MW_aa = {
-		A:32,
-		R:102,
-		N:136,
-		D:58,
-		C:225,
-		Q:142,
-		E:78,
-		G:21,
-		H:5125,
-		I:45,
-		L:45,
-		K:41,
-		M:980,
-		F:5200,
-		P:2675,
-		S:34,
-		T:41,
-		W:29050,
-		Y:5375,
-		V:43};
+//Molecular weights of individual amino acids
+const MW_AA = {
+		A:71.037114,
+		R:156.101111,
+		N:114.042927,
+		D:115.026943,
+		C:103.009185,
+		Q:128.058578,
+		E:129.042593,
+		G:57.021464,
+		H:137.058912,
+		I:113.084064,
+		L:113.084064,
+		K:128.094963,
+		M:131.040485,
+		F:147.068414,
+		P:97.052764,
+		S:87.032028,
+		T:101.047679,
+		W:186.079313,
+		Y:163.06332,
+		V:99.068414};
 
+// Calculates molecular weight from a given sequence
 function calculateMW (sequence) {
 	let MW = 0;
 	sequence = sequence.toUpperCase();
-	for (var i = 0; i < sequence.length; i++) {
+	for (let i = 0; i < sequence.length; i++) {
 		aa = sequence.charAt(i);
-		if (MW_aa[aa] != undefined) {
-			MW += eAA[aa];
+		if (MW_AA[aa] != undefined) {
+			MW += MW_AA[aa];
 		}
 	}
 	return MW;
 }
-
 
 
 //Extinction coefficient of a peptide bond, M-1*cm-1
@@ -60,13 +61,14 @@ const eAA = {
 		Y:5375,
 		V:43};
 
-eeP_Nterm = 30;
+// N-terminal proline contribution		
+const eeP_Nterm = 30;
 
 function calculateEcoeff214nm (sequence) {
-	E = 0;
+	let E = 0;
 	previousAA = false;
 	sequence = sequence.toUpperCase();
-	for (var i = 0; i < sequence.length; i++) {
+	for (let i = 0; i < sequence.length; i++) {
 		aa = sequence.charAt(i);
 		if (eAA[aa] != undefined) {
 			if (aa == 'P' & !previousAA) {
